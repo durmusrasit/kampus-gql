c16a17e96ab390baccf819b114776b1761ce9d55
durmusrasit <12167194+durmusrasit@users.noreply.github.com>
2023-01-30 18:17:46 +0300

    BREAKING CHANGE: Rename type and field names in schema And add `GetPostInput` input type
    Use the PanoAPI client
    And remove unused functions & structs

--- query/post_query.go
+++ query/post_query.go
@@ -2,55 +2,62 @@ package query
 
 import (
 	"context"
-	"errors"
-	"strings"
 
-	"github.com/durmusrasit/kampus-gql/models"
+	pano_api "github.com/durmusrasit/kampus-gql/rpc/pano-api"
 )
 
-type PostInput struct {
+type CreatePostInput struct {
 	Title   string
 	Url     string
 	Content *string
 	UserId  string
 }
 
-func (q *Query) CreatePost(ctx context.Context, args *struct {
-	Input *PostInput
-}) (*postResolver, error) {
-
-	post := &models.Post{
-		Title:   args.Input.Title,
-		Url:     args.Input.Url,
-		Content: *args.Input.Content,
-		Slug:    strings.ToLower(args.Input.Title),
-		UserID:  args.Input.UserId,
-	}
-
-	return &postResolver{post}, nil
+type CreatePostArgs struct {
+	Input *CreatePostInput
 }
 
-func (q *Query) Posts(ctx context.Context) (*[]*postResolver, error) {
-
-	var batch []*postResolver
-
-	return &batch, nil
-}
-
-func (q *Query) Post(ctx context.Context, args struct {
-	Slug string
-	Id   string
-}) (*postResolver, error) {
-	post, err := q.GetPost(ctx, args.Slug, args.Id)
+func (q *Query) CreatePost(ctx context.Context, args *CreatePostArgs) (*postResolver, error) {
+	createdPost, err := q.Clients.PanoAPI.CreatePost(ctx, &pano_api.CreatePostRequest{Title: args.Input.Title, Url: args.Input.Url, Content: *args.Input.Content, UserId: args.Input.UserId})
 
 	if err != nil {
 		return nil, err
 	}
 
-	return &postResolver{post}, nil
+	return &postResolver{createdPost.Post}, nil
 }
 
-func (q *Query) GetPost(ctx context.Context, Slug string, Id string) (*models.Post, error) {
+type GetPostInput struct {
+	Slug string
+	Id   string
+}
 
-	return nil, errors.New("Post not found")
+type GetPostArgs struct {
+	Input *GetPostInput
+}
+
+func (q *Query) Post(ctx context.Context, args *GetPostArgs) (*postResolver, error) {
+	response, err := q.Clients.PanoAPI.GetBatchPosts(ctx, &pano_api.GetBatchPostsRequest{Ids: []string{args.Input.Id}})
+
+	if err != nil {
+		return nil, err
+	}
+
+	return &postResolver{response.Posts[0]}, nil
+}
+
+func (q *Query) Posts(ctx context.Context) (*[]*postResolver, error) {
+	response, err := q.Clients.PanoAPI.GetPosts(ctx, &pano_api.GetPostsRequest{})
+
+	if err != nil {
+		return nil, err
+	}
+
+	var batch []*postResolver
+	for _, p := range response.Posts {
+		post := postResolver{p}
+		batch = append(batch, &post)
+	}
+
+	return &batch, nil
 }
--- query/post_resolver.go
+++ query/post_resolver.go
@@ -1,7 +1,21 @@
 package query
 
-import "github.com/durmusrasit/kampus-gql/models"
+import (
+	pano_api "github.com/durmusrasit/kampus-gql/rpc/pano-api"
+)
 
 type postResolver struct {
-	post *models.Post
+	post *pano_api.Post
 }
+
+func (r *postResolver) ID() string { return r.post.Id }
+
+func (r *postResolver) Title() string { return r.post.Title }
+
+func (r *postResolver) Url() string { return r.post.Url }
+
+func (r *postResolver) Content() *string { return &r.post.Content }
+
+func (r *postResolver) Slug() string { return r.post.Slug }
+
+func (r *postResolver) UserID() string { return r.post.UserId }
